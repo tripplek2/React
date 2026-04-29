930256ba0b0bbf929d6bacb006337236925bff32
Kelvin Korir <kibetkelvin222@gmail.com>
2026-04-29 20:30:57 +0300

    test file created

--- blogsite/src/App.test.jsx
+++ blogsite/src/App.test.jsx
@@ -1,4 +1,6 @@
+import { test, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import App from "./App";
 
 test("renders blog name", () => {
