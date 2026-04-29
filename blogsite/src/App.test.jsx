46fe0b9bc9146e89b92c8f7eff895b04bdfbec10
Kelvin Korir <kibetkelvin222@gmail.com>
2026-04-29 21:10:21 +0300

    added import from react

--- blogsite/src/App.test.jsx
+++ blogsite/src/App.test.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { test, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
@@ -8,5 +9,5 @@ expect.extend(matchers);
 test("renders blog name", () => {
     render(<App />);
     const title = screen.getByText(/kelvin's Software Development Blog/i);
-    expect(title).toBeInTheDocument();
+    expect(title).toBeTruthy();
 });
